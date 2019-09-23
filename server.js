6baa6c0ba1dac8c21f3cd02b23fb39ecc85c073e
gaorock530 <gaorock530@hotmail.com>
2019-09-24 06:04:04 +0800

    fix

--- server.js
+++ server.js
@@ -15,3 +15,28 @@ app.prepare()
     console.log('> Ready on http://localhost:3000');
   });
 });
+
+// const { createServer } = require('https')
+// // const { parse } = require('url')
+// const next = require('next')
+// const express = require('express')
+// ​
+// const dev = process.env.NODE_ENV !== 'production'
+// const app = next({ dev })
+// const handle = app.getRequestHandler()
+
+// const options = {
+//   key: fs.readFileSync(path.join(__dirname, '/ssl/www/2_www.yingxitech.com.key')),
+//   cert: fs.readFileSync(path.join(__dirname, '/ssl/www/1_www.yingxitech.com_bundle.crt'))
+// };
+// ​
+// app.prepare().then(() => {
+//   const server = express();
+//   server.get('*', (req, res) => {
+//     return handle(req, res);
+//   });
+//   createServer(options, server).listen(3000, err => {
+//     if (err) throw err
+//     console.log('> Ready on http://localhost:3000')
+//   })
+// })
