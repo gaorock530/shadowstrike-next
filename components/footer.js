b330204db5d4c3473a6ddcc59fd4f28db00e418f
gaorock530 <gaorock530@hotmail.com>
2019-09-24 14:45:06 +0800

    add link

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ export default () => {
     <footer>
       <h5>联系方式：yingxitech@qq.com</h5>
       <p>2017-2019 ShadowStrike Ltd. 影袭科技 &copy; All rights reserved. </p>
-      <p>豫ICP备18021827号-1</p>
+      <a href="http://www.beian.miit.gov.cn/state/outPortal/loginPortal.action" target="_blank" rel="noopener noreferrer">豫ICP备18021827号-1</a>
     </footer>
   )
 }
