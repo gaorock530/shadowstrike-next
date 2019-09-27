254ecb03e43075d975daa6f7d51d99e7c99de0a5
gaorock530 <gaorock530@hotmail.com>
2019-09-27 20:57:41 +0800

    fix

---
+++ pages/baoming.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Head from 'next/head'
+import Body from '../components/body'
+
+
+
+const Baoming = () => (
+  <div>
+    <Head>
+      <title>报名通道</title>
+      <meta name="description" content="中原青少年艺术赛事网"/>
+      <meta name="keywords" content="中原青少年艺术赛事网 中原 青少年 艺术 赛事" />
+    </Head>
+    <Body>
+      <div className="top-content">
+        报名通道即将开启
+      </div>
+      
+    </Body>
+    
+  </div>
+)
+
+export default Baoming
