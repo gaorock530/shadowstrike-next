2de9da150acda9b8cdb682548c6362d52f983a11
gaorock530 <gaorock530@hotmail.com>
2019-09-28 02:33:18 +0800

    edit baoming.js

--- pages/baoming.js
+++ pages/baoming.js
@@ -20,15 +20,14 @@ class Baoming extends React.PureComponent {
           <meta name="description" content="中原青少年艺术赛事网"/>
           <meta name="keywords" content="中原青少年艺术赛事网 中原 青少年 艺术 赛事" />
         </Head>
-        <Body>
-          <h5>你好，{this.props.query.nickname}</h5>
-          <h5>欢迎来到中原青少年艺术赛事网</h5>
 
-          <div className="user-icon" style={{backgroundImage: `url('${this.props.query.pic || '/static/pic/back.jpeg'}')`}}></div>
-          <h3>
-            报名通道将在10月1日开启
-          </h3>
-        </Body>
+        <h5>你好，{this.props.query.nickname}</h5>
+        <h5>欢迎来到中原青少年艺术赛事网</h5>
+
+        <div className="user-icon" style={{backgroundImage: `url('${this.props.query.pic || '/static/pic/back.jpeg'}')`}}></div>
+        <h3>
+          报名通道将在10月1日开启
+        </h3>
         
       </div>
     )
