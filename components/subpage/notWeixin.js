a256fb188447b32aaeb0fb938da67329fb581357
gaorock530 <gaorock530@hotmail.com>
2019-10-01 07:24:10 +0800

    pay

---
+++ components/subpage/notWeixin.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../../styles/form.scss';
+import Head from 'next/head'
+import Meta from '../bisaiMeta'
+
+export default () => {
+  return (
+    <div className="form-component-wrapper notweixin">
+      <Meta/>
+      <Head>
+          <title>报名通道</title>
+      </Head>
+      <h2>欢迎来到中原青少年艺术赛事网</h2>
+
+      <h4>报名指引</h4>
+      <p>第一步：长按下方二维码，进入公众号</p>
+      <p>第二步：关注公众号</p>
+      <p>第三步：点击 ‘我要参加’ - ‘报名’</p>
+      <img src="/qrcode_for_gh_17a95697c75e_344.jpg"/>
+    </div>
+  )
+};
+
+
+
