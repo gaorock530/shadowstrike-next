574f8fd4917483937f658e3b4cc590d77f8dd377
gaorock530 <gaorock530@hotmail.com>
2019-10-06 00:23:02 +0800

    fix

--- pages/baoming.js
+++ pages/baoming.js
@@ -56,9 +56,6 @@ class Baoming extends React.PureComponent {
           imgUrl: icon_url, // 分享图标
         })
       });
-      wx.error(function(res){
-        // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
-      });
     }catch(e) {
       console.log(e)
     }
@@ -180,7 +177,7 @@ async function getConfig () {
     return JSON.stringify({type: 'request error', error: e});
   }
   wx.config({
-    debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
     appId, // 必填，公众号的唯一标识
     timestamp, // 必填，生成签名的时间戳
     nonceStr: noncestr, // 必填，生成签名的随机串
