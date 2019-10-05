28ca46bbbb87a892b6b85c5e1551de50690716fa
gaorock530 <gaorock530@hotmail.com>
2019-10-06 00:21:11 +0800

    fix

--- pages/baoming.js
+++ pages/baoming.js
@@ -40,6 +40,28 @@ class Baoming extends React.PureComponent {
   }
 
   async componentDidMount() {
+    try {
+      await getConfig();
+      wx.ready(function(){
+        // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+        wx.updateAppMessageShareData({ 
+          title: page.shareTitle, // 分享标题
+          desc: page.shareDesc, // 分享描述
+          link: page.shareLink, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+          imgUrl: icon_url, // 分享图标
+        });
+        wx.updateTimelineShareData({ 
+          title: page.shareTitle, // 分享标题
+          link: page.shareLink, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+          imgUrl: icon_url, // 分享图标
+        })
+      });
+      wx.error(function(res){
+        // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+      });
+    }catch(e) {
+      console.log(e)
+    }
 
     if (!this.props.query.unionid || this.props.query.unionid === 'undefined') return;
 
@@ -84,28 +106,7 @@ class Baoming extends React.PureComponent {
     
     
 
-    try {
-      await getConfig();
-      wx.ready(function(){
-        // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
-        wx.updateAppMessageShareData({ 
-          title: page.shareTitle, // 分享标题
-          desc: page.shareDesc, // 分享描述
-          link: page.shareLink, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-          imgUrl: icon_url, // 分享图标
-        });
-        wx.updateTimelineShareData({ 
-          title: page.shareTitle, // 分享标题
-          link: page.shareLink, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-          imgUrl: icon_url, // 分享图标
-        })
-      });
-      wx.error(function(res){
-        // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
-      });
-    }catch(e) {
-      console.log(e)
-    }
+    
   }
 
   onConfirm = (formData) => {
@@ -179,7 +180,7 @@ async function getConfig () {
     return JSON.stringify({type: 'request error', error: e});
   }
   wx.config({
-    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+    debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
     appId, // 必填，公众号的唯一标识
     timestamp, // 必填，生成签名的时间戳
     nonceStr: noncestr, // 必填，生成签名的随机串
